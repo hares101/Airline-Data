73af64690323b3dc2705947d7f54920d9b4f80f8
Hares <hrahimzei@gmail.com>
2014-10-06 12:53:14 -0700

    question 4

--- hw1.R
+++ hw1.R
@@ -43,4 +43,43 @@ table2 =table1[c('ATL', 'ORD', 'DFW','DEN','LAX','IAH','PHX','SFO','CLT','LAS'
        c('WN','EV','DL','OO','AA','UA','MQ','US','B6','FL')]
 table2
 
-##mean delay
+##mean delay question 4
+
+names(data)
+data$MONTH
+  #dep_delay
+dep_delay_nov = mean(data[data$MONTH == 11,'DEP_DELAY'], na.rm=TRUE)
+dep_delay_dec = mean(data[data$MONTH == 12, 'DEP_DELAY'], na.rm=TRUE)
+  #arr_delay
+arr_delay_nov = mean(data[data$MONTH == 11,'ARR_DELAY'], na.rm=TRUE)
+arr_delay_dec = mean(data[data$MONTH == 12,'ARR_DELAY'], na.rm=TRUE)
+  #carrier_delay
+carrier_delay_nov = mean(data[data$MONTH == 11,'CARRIER_DELAY'], na.rm=TRUE)
+carrier_delay_dec = mean(data[data$MONTH == 12,'CARRIER_DELAY'], na.rm=TRUE)
+  #weather_delay
+weather_delay_nov = mean(data[data$MONTH == 11,'WEATHER_DELAY'], na.rm=TRUE)
+weather_delay_dec = mean(data[data$MONTH == 12,'WEATHER_DELAY'], na.rm=TRUE)
+  #nas_delay
+nas_delay_nov = mean(data[data$MONTH == 11,'NAS_DELAY'], na.rm=TRUE)
+nas_delay_dec = mean(data[data$MONTH == 12,'NAS_DELAY'], na.rm=TRUE)
+  #security_delay
+sec_delay_nov = mean(data[data$MONTH == 11,'SECURITY_DELAY'], na.rm=TRUE)
+sec_delay_dec = mean(data[data$MONTH == 12,'SECURITY_DELAY'], na.rm=TRUE)
+  #late aircraft delay
+late_delay_nov = mean(data[data$MONTH == 11,'LATE_AIRCRAFT_DELAY'], na.rm=TRUE)
+late_delay_dec = mean(data[data$MONTH == 12,'LATE_AIRCRAFT_DELAY'], na.rm=TRUE)
+
+arr_delay_dec
+arr_delay_nov
+
+mean_delay_nov =mean(dep_delay_nov, arr_delay_nov,
+     carrier_delay_nov, weather_delay_nov,
+     nas_delay_nov, sec_delay_nov, late_delay_nov)
+mean_delay_dec =mean(dep_delay_dec, arr_delay_dec,
+     carrier_delay_dec, weather_delay_dec,
+     nas_delay_dec, sec_delay_dec, late_delay_dec)
+mean_delay_nov
+mean_delay_dec
+
+#question 5#
+hist(data[data$MONTH == 11, 'LATE_AIRCRAFT_DELAY'], na.rm=TRUE)
